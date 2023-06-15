074b369478294a61b56c80d29c5c74daa1ef5371
ducanh2710203 <duc2710203@gmail.com>
2023-06-15 15:22:48 +0700

    cm

---
+++ index.ts
@@ -0,0 +1,40 @@
+
+import express from "express";
+
+import bodyParser from "body-parser";
+
+import mongoose from "mongoose";
+
+
+
+import bookRoutes from './src/router/book.router';
+
+
+
+const PORT = 3216;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.set('views', './src/views');
+
+const DB_URL = 'mongodb://127.0.0.1:27017/bookManager';
+
+mongoose.connect(DB_URL)
+
+    .then(() => console.log('DB Connected!'))
+
+    .catch(error => console.log('DB connection error:', error.message));
+
+app.use(bodyParser.json());
+
+
+
+app.use('/book', bookRoutes);
+
+app.listen(PORT, () => {
+
+    console.log(`http://localhost:${PORT}`);
+
+})
